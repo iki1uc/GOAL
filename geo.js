b3935164e64c4a1620b461ccbb4dff76b833af66
iki1uc <entertrainer@web.de>
2026-09-03 13:31:45 +0200

    Update geo.js

--- geo.js
+++ geo.js
@@ -1,22 +1,32 @@
-// 81_81_1 / geo.js
-// Minimal: nur Formeln, keine Visualisierung
+export function geoAxes(Phi, phi, phi2, phiinfty, cplus = 1.0) {
 
-export function geoAxes(Phi, phi, phi2, phiinfty) {
+    const cPhi      = Phi      * cplus;
+    const cphi      = phi      * cplus;
+    const cphi2     = phi2     * cplus;
+    const cphiinfty = phiinfty * cplus;
+
     return {
-        Phi,
-        phi,
-        phi2,
-        phiinfty,
-        avg: (Phi + phi + phi2 + phiinfty) / 4
+        Phi: cPhi,
+        phi: cphi,
+        phi2: cphi2,
+        phiinfty: cphiinfty,
+        avg: (cPhi + cphi + cphi2 + cphiinfty) / 4,
+        cplus
     };
 }
 
-export function geoLAGE(Phi, phi, phi2, phiinfty) {
+export function geoLAGE(Phi, phi, phi2, phiinfty, cplus = 1.0) {
+
+    const cPhi      = Phi      * cplus;
+    const cphi      = phi      * cplus;
+    const cphi2     = phi2     * cplus;
+    const cphiinfty = phiinfty * cplus;
+
     return {
-        x: Phi * 9,
-        y: phi * 9,
-        z: phiinfty * 9,
-        stabil: phi2
+        x: cPhi * 9,
+        y: cphi * 9,
+        z: cphiinfty * 9,
+        stabil: cphi2,
+        cplus
     };
 }
-
