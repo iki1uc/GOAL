3eaf87a350df4fb25e3e0e3716de84ab5ae6ebde
iki1uc <entertrainer@web.de>
2026-09-03 12:32:19 +0200

    Implement Arbeit class with pipeline methods

---
+++ arbei.js
@@ -0,0 +1,71 @@
+class Arbeit {
+
+    constructor(){
+        this.stage = 3;          // Startstage
+        this.mode = "CALL";      // CALL oder RECALL
+        this.pipe = null;        // Pipeline-Name
+        this.result = null;      // Ergebnis
+    }
+
+    // Stage setzen
+    setStage(stage){
+        this.stage = stage;
+    }
+
+    // CALL-Pipelines
+    runCall(pipeName){
+        this.mode = "CALL";
+        this.pipe = pipeName;
+
+        switch(pipeName){
+            case "HWpipeline":
+                this.result = Call.pipeline(this.stage);
+                break;
+
+            case "HWpipeline1":
+                this.result = Call.pipeline1(this.stage);
+                break;
+
+            case "HWpipeline12":
+                this.result = Call.pipeline12(this.stage);
+                break;
+
+            default:
+                throw new Error(`Unbekannte CALL-Pipeline: ${pipeName}`);
+        }
+
+        return this.result;
+    }
+
+    // RECALL-Pipelines
+    runRecall(pipeName){
+        this.mode = "RECALL";
+        this.pipe = pipeName;
+
+        switch(pipeName){
+            case "HWpipeline3":
+                this.result = Recall.pipeline3(this.stage);
+                break;
+
+            case "HWpipeline6":
+                this.result = Recall.pipeline6(this.stage);
+                break;
+
+            case "HWpipeline9":
+                this.result = Recall.pipeline9(this.stage);
+                break;
+
+            default:
+                throw new Error(`Unbekannte RECALL-Pipeline: ${pipeName}`);
+        }
+
+        return this.result;
+    }
+
+    // Ergebnis abrufen
+    getResult(){
+        return this.result;
+    }
+}
+
+window.Arbeit = new Arbeit();
