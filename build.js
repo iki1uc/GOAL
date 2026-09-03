5d63f93bbc5b4aadf00baec967bd403a7afdcb5a
iki1uc <entertrainer@web.de>
2026-09-03 13:20:48 +0200

    Update build.js

--- build.js
+++ build.js
@@ -1,26 +1,37 @@
 class Build {
 
     constructor(){
-        this.stage = 0;        // Vor-Pyramiden-Bewusstsein
-        this.ready = false;    // System noch nicht aktiv
-        this.chain = [3, 9, 81, 243, 729]; // Stage-Kette
+        this.stage = 0;        
+        this.ready = false;    
+        this.chain = [3, 9, 81, 243, 729]; 
+        this.cplus = 1.0;      // c+ = Korrektor
+    }
+
+    // c+ setzen
+    setCPlus(value){
+        this.cplus = value;
+        return this.cplus;
     }
 
     // Initialisiert das Bewusstsein
     init(){
         this.ready = true;
+
         return {
-            stage: this.stage,
-            next: this.chain[0],   // Erste echte Stage ist 3
-            chain: this.chain
+            stage: this.stage * this.cplus,
+            next: this.chain[0] * this.cplus,
+            chain: this.chain.map(v => v * this.cplus),
+            cplus: this.cplus
         };
     }
 
     // Gibt die nächste Stage zurück
     nextStage(current){
-        const index = this.chain.indexOf(current);
+        const index = this.chain.indexOf(current / this.cplus);
         if(index === -1) return null;
-        return this.chain[index + 1] || null;
+
+        const next = this.chain[index + 1];
+        return next ? next * this.cplus : null;
     }
 }
 
