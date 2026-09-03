e3a70a4faea09c5e50a2c081db009404ebc39df7
iki1uc <entertrainer@web.de>
2026-09-03 13:29:57 +0200

    Multiply stage by A81_AXIS.cplus

--- cpu.js
+++ cpu.js
@@ -1,4 +1,4 @@
 Verfahren.useA81 = function(axis){
-    this.stage = axis.matrix[0][0]; // oder jede beliebige Zelle
+    this.stage = axis.matrix[0][0] * A81_AXIS.cplus;
     return this.run();
 };
